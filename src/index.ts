b90a66e5f72d62b150bddbaf6521c624605e6842
Eric Furspan <ericfurspan@gmail.com>
2023-08-05 15:15:02 -0400

    setup for local ts-node

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import fetchMetrics from './fetchMetrics.js';
 
+const API_RATE_LIMIT = 5;
+const REQUESTS_PER_ITEM = 2;
+const TIME_BETWEEN_REQUESTS = (60 * 1000) / API_RATE_LIMIT; // in milliseconds
+
 const tickers = [
   'AAPL',
   'TSLA',
@@ -23,4 +27,15 @@ const tickers = [
   'HSY'
 ];
 
-fetchMetrics(tickers[3], true);
+async function process(tickers: string[]) {
+  for (const ticker of tickers) {
+    for (let i = 0; i < REQUESTS_PER_ITEM; i++) {
+      await fetchMetrics(ticker, true);
+
+      // pause between requests
+      await new Promise((resolve) => setTimeout(resolve, TIME_BETWEEN_REQUESTS));
+    }
+  }
+}
+
+process(tickers);
