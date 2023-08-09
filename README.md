b6d74e113cf5cc30db0446effa2929f35e0eacd6
Eric Furspan <ericfurspan@gmail.com>
2023-08-08 21:25:32 -0400

    allow for both json and html results

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # ticker-lookup
 
- A nodejs/typescript wrapper around select AlphaVantage endpoints to enable rapid access to key financial metrics for any stock. 
+ A nodejs/typescript wrapper around select AlphaVantage endpoints to enable rapid access to key financial metrics for any stock.
+
+ ## Sample Usage
+
+*These examples will use Microsoft (symbol: MSFT) for the lookup.*
+
+Get results in JSON format: (i.e for script usage)
+
+`https://ticker-lookup.netlify.app/.netlify/functions/lookup?symbol=MSFT`
+
+
+Get results in HTML format: (i.e for browser usage)
+
+`https://ticker-lookup.netlify.app/.netlify/functions/lookup?symbol=MSFT&format=html`
+
 
 ## Resources
 - https://www.alphavantage.co/documentation/
--- netlify/functions/lookup.ts
+++ netlify/functions/lookup.ts
@@ -5,6 +5,8 @@ import { compileKeyMetricsWithNames } from '../../src/utils.js';
 export async function handler(event: HandlerEvent) {
   const params = event.queryStringParameters;
 
+  const format = params?.format ?? 'html';
+
   if (!params?.symbol)
     return { statusCode: 400, body: 'missing "symbol" parameter \n\n\texample: ?symbol=AAPL' };
 
@@ -13,6 +15,17 @@ export async function handler(event: HandlerEvent) {
   const keyMetrics = compileKeyMetricsWithNames(overviewResult, quoteResult);
   const rawData = { ...overviewResult, ...quoteResult };
   const lastUpdated = new Date(keyMetrics['Quote Date']).toLocaleString();
+
+  if (format === 'json') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ ...keyMetrics, ...rawData })
+    };
+  }
 
   return {
     statusCode: 200,
