3b796a7abb224a60ec0490b07eed5092c8cc65f9
Eric Furspan <ericfurspan@gmail.com>
2023-08-20 15:15:30 -0400

    update error handling

--- netlify/functions/lookup.ts
+++ netlify/functions/lookup.ts
@@ -1,6 +1,6 @@
 import { HandlerEvent } from '@netlify/functions';
-import fetchTicker from '../../src/fetchTicker.js';
 import { compileKeyMetricsWithNames } from '../../src/utils.js';
+import fetchTicker from '../../src/fetchTicker.js';
 
 export async function handler(event: HandlerEvent) {
   const params = event.queryStringParameters;
@@ -10,7 +10,11 @@ export async function handler(event: HandlerEvent) {
   if (!params?.symbol)
     return { statusCode: 400, body: 'missing "symbol" parameter \n\n\texample: ?symbol=AAPL' };
 
-  const { overviewResult, quoteResult } = await fetchTicker(params.symbol);
+  const response = await fetchTicker(params.symbol);
+
+  if ('error' in response) return { statusCode: 200, body: response.error };
+
+  const { quoteResult, overviewResult } = response;
 
   const keyMetrics = compileKeyMetricsWithNames(overviewResult, quoteResult);
   const rawData = { ...overviewResult, ...quoteResult };
@@ -38,12 +42,12 @@ export async function handler(event: HandlerEvent) {
       <head>
         <meta charset="utf-8">
       </head>
-      <body style="font-size: larger">
+      <body style="font-size: larger; font-family: -apple-system, BlinkMacSystemFont, Helvetica, Arial, sans-serif">
         <h1>${rawData.Name} (${params.symbol.toUpperCase()})</h1>
 
         <p>Last updated: ${lastUpdated}</p>
 
-        <details>
+        <details open>
           <summary>Key Metrics</summary>
           <pre>${JSON.stringify(keyMetrics, null, 4)}</pre>
         </details>
--- src/fetchTicker.ts
+++ src/fetchTicker.ts
@@ -8,21 +8,24 @@ const queryAlphaVantage = async (ticker: string, queryFn: QueryFunction) => {
   const query = `${BASE_QUERY}&symbol=${ticker}&function=${queryFn}`;
   const response = await fetch(query);
 
-  if (response.ok) {
-    const data = await response.json();
+  const data = await response.json();
 
-    return data as QueryResult;
-  }
-
-  console.error(`${response.status} - ${response.statusText}`, query);
-  throw new Error(`queryAlphaVantage error`);
+  return data as QueryResult | { [key: string]: string };
 };
 
 async function fetchTicker(ticker: string) {
-  const quoteResult = (await queryAlphaVantage(ticker, 'GLOBAL_QUOTE')) as GlobalQuoteResult;
-  const overviewResult = (await queryAlphaVantage(ticker, 'OVERVIEW')) as OverviewResult;
+  const quoteResult = await queryAlphaVantage(ticker, 'GLOBAL_QUOTE');
+  const overviewResult = await queryAlphaVantage(ticker, 'OVERVIEW');
 
-  return { quoteResult, overviewResult };
+  if ('Note' in quoteResult) return { error: quoteResult.Note };
+  if ('Information' in quoteResult) return { error: quoteResult.Information };
+  if ('Note' in overviewResult) return { error: overviewResult.Note };
+  if ('Information' in overviewResult) return { error: overviewResult.Information };
+
+  return { quoteResult, overviewResult } as {
+    quoteResult: GlobalQuoteResult;
+    overviewResult: OverviewResult;
+  };
 }
 
 export default fetchTicker;
